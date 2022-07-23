e7d556b6420f6fcf5037d634804fa53e750c4f33
dox-net <85663797+dox-net@users.noreply.github.com>
2022-07-23 01:07:00 -0400

    Add files via upload

---
+++ scripts/direct/NIGHTMODEHAXXV2.py
@@ -0,0 +1,28 @@
+import time
+import requests
+import webbrowser
+from random import randint
+
+
+ip = requests.get("https://api.ipify.org").text
+y = randint(10, 25)
+
+print("NIGMODE HAXX V2")
+question = input("Type 1 to start: ")
+if question == "1":
+ print("Loading NIGMODE HAxx V2...")
+time.sleep(2)
+print("Checking...")
+time.sleep(2)
+print("Organizing files...")
+time.sleep(2)
+print("Installing", y, "Modules...")
+time.sleep(5)
+print("Request blocked by firewall.")
+time.sleep(1)
+print("Attempting to Bypass firewall...")
+time.sleep(10)
+print("Attempt Failed!")
+time.sleep(0.5)
+print("Sorry, but the tool you are using is ILLEGAL and your IP address (",ip,") has been sent to the CIA and MOSSAD for further investigation.")
+time.sleep(30)
